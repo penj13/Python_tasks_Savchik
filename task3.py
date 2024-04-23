7093c04fe846c365c1662b97d648609381f96d51
penj13 <104915075+penj13@users.noreply.github.com>
2024-04-23 11:26:57 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -3,5 +3,5 @@ d1 = datetime.strptime('1/1/2020', '%m/%d/%Y')
 d2 = datetime.strptime('12/2/2019', '%m/%d/%Y')
 def dayscount (d1, d2):
     n_days = abs((d2 - d1).days)
-    print (n_days)
+    return n_days
 dayscount (d1, d2)
