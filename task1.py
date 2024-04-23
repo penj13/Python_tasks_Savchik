a153fc35d564bad60ce72ba4c975c87717afb774
penj13 <104915075+penj13@users.noreply.github.com>
2024-04-23 10:25:31 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,12 +1,6 @@
 import random
 m = random.choices(range(1, 1000, 1), k = 20)
 def uni_mass(m):
-    uni_f = []
-    uni = []
-    for x in m:
-        if x not in uni:
-            uni.append(x)
-    uni_f = sorted(uni)
-    print (uni_f)
+    uni_f = sorted(set(m))
+    return uni_f
 uni_mass(m)
- 
