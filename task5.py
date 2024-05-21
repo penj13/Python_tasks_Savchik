3bdbdfc7ba72973640229b27538a65c099fbdbe6
penj13 <104915075+penj13@users.noreply.github.com>
2024-05-21 12:57:44 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -2,30 +2,33 @@ search_words = ["watch new movies", "coffee near me", "how to find the determina
 
 
 def percent_stats(count_words: str):
-    list_of_searches = []
-    dict_search_words = dict.fromkeys(count_words)
+#   формируем словарь из списка запросов
+    dict_search = dict.fromkeys(count_words)
+    num_of_searches = len(dict_search)
+    num_of_values = []
     
-    for key, value in dict_search_words.items():
+#   формируем список количества слов каждого запроса
+    for key, value in dict_search.items():
         spaces_key = key.split()
         value = len(spaces_key)
-        list_of_searches.append(value)
+        num_of_values.append(value)
 
-    num_of_searches = len(list_of_searches)
-    max_words_in_one_search = max(list_of_searches)
-    
-    for word_count in range(max_words_in_one_search+1):
-        num_of_words = list_of_searches.count(word_count)
+#   проверка
+#   print(num_of_values)
+#   print(max(num_of_values))
+
+#   высчитываем процентное соотношение запросов с определенным количеством слов по отношению ко числу всех запросов и выводим на экран
+    for word_count in range(1, max(num_of_values) + 1):
+        num_of_words = num_of_values.count(word_count)
         percent_of_searches = num_of_words / num_of_searches
         if num_of_words > 0:
             if word_count % 10 in (2, 3, 4):
-                i = "а"
+                i = "слова"
             elif word_count % 10 in (0, 5, 6, 7, 8, 9):
-                i = ""
+                i = "слов"
             elif word_count % 10 in (1,):
-                i = "о"
-            print(f"{word_count} слов{i}: {percent_of_searches:.2%}")
+                i = "слово"
+            print(f"{word_count} {i}: {percent_of_searches:.2%}")
 
 
 percent_stats(search_words)
-
-
