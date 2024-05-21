0128dcaeabce24f2080e1f2548901ef4238a1810
penj13 <104915075+penj13@users.noreply.github.com>
2024-05-21 17:27:08 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,54 +1,35 @@
 from math import sqrt
-a = []
-list_1 = int(input("Введите количество чисел 1 строки: "))
-for z in range(list_1):
-    new_n = int(input("Введите число: "))
-    a.append(new_n)
-print('Строка а - ',a)
 
-b = []
-list_2 = int(input("Введите количество чисел 2 строки: "))
-for z in range(list_2):
-    new_n = int(input("Введите число: "))
-    b.append(new_n)
-print('Строка b - ',b)
+vector_a = [1, 320, -500]
+vector_b = [-100, 0, 0]
+num_of_values_a = len(vector_a)
+num_of_values_b = len(vector_b)
 
-def task2 (a, b):
-    if sum(a) == 0:
-         print ('Нулевые данные проверить невозможно')
-    elif sum(b) == 0:
-         print ('Нулевые данные проверить невозможно')
-    else:
-        if list_1 > list_2:
-            min = list_1 - list_2
-            b2 = [0]*min
-            b.extend(b2)
-        elif list_2 > list_1:
-            min = list_2 - list_1
-            a2 = [0]*min
-            a.extend(a2)
-        
+
+def cosine_similarity (a, b):
+    if num_of_values_a == num_of_values_b:
         i = 0
-        sc = 0
-        norm_a = 0
-        norm_b = 0
-        for z in range(list_1):
-            sc_1 = a[i]*b[i]
-            sc = sc_1 + sc
-            norm_1_a = a[i]**2
-            norm_a = norm_1_a + norm_a
-            norm_1_b = b[i]**2
-            norm_b = norm_1_b + norm_b
+        general_dot = 0
+        general_norm_a = 0
+        general_norm_b = 0
+        for count in range(num_of_values_a):
+            dot_i = a[i]*b[i]
+            general_dot += dot_i
+            norm_a_i = a[i]**2
+            general_norm_a += norm_a_i
+            norm_b_i = b[i]**2
+            general_norm_b += norm_b_i
             i += 1
-        norm_f = sqrt(norm_a)*sqrt(norm_b)
-        cos_sim = sc/norm_f
-        if cos_sim < 0:
-            print ('Косинус не может быть меньше 0 - сходство нулевое')
+        general_norm = sqrt(general_norm_a)*sqrt(general_norm_b)
+        cos_sim = general_dot/general_norm
+        if cos_sim < -1:
+            print ('Косинус не может быть меньше -1')
         elif cos_sim > 1:
             print ('Косинус не может быть больше 1')
         else: 
             print (cos_sim)
-task2 (a, b)
+    else:
+        print ('Разное количество значений в векторах')
 
 
-
+cosine_similarity (vector_a, vector_b)
