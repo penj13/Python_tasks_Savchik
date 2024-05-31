60336fe031934ee3cbceec9e626b3fc349784afd
penj13 <104915075+penj13@users.noreply.github.com>
2024-05-31 04:03:29 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,133 +1,70 @@
-# возможно это задание по JS все-таки ??? :)
 class Node:
     def __init__(self, data):
-        self.data = data 
-        self.next_node = None 
-        self.prev_node = None
-        
-class MyList:
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class NodeList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.i = 0
-    
-    def menu(self):
-        print('Что делаем со списком?')
-        print('1. Добавить элемент в конец списка')
-        print('2. Удалить элемент из конца списка')
-        print('3. Добавить элемент в начало списка')
-        print('4. Удалить элемент из начала списка')
-        print('5. Показать список')
-        a = input('Введите цифру действия:', )
-        if a == '1':
-            b = input('Введите значение узла:', )
-            list.add_f(b)
-        elif a == '2':
-            i = -1
-            list.del_f(i)
-        elif a == '3':
-            b = input('Введите значение узла:', )
-            list.add_s(b)
-        elif a == '4':
-            i = -1
-            list.del_s(i)
-        elif a == '5':
-            list.display()
-    
-    def add_f (self, data):
-        new_node = Node(data)
-        if self.head == None:
-            self.i += 1
-            self.head = self.tail = new_node
-            self.head.prev_node = None
-            self.tail.next_node = None
-            print('-----------------------------------')
-            list.menu()
 
+    def push(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = self.tail = new_node
+            self.head.next = self.head.prev = self.tail.next = self.tail.prev = None
         else:
-            self.i += 1
-            self.tail.next_node = new_node
-            new_node.prev_node = self.tail
+            self.tail.next = new_node
+            new_node.prev = self.tail
             self.tail = new_node
-            self.tail.next_node = None
-            print('-----------------------------------')
-            list.menu()
-            
-    def add_s (self, data):
+            self.tail.next = None
+    
+    def unshift(self, data):
         new_node = Node(data)
-        if self.head == None:
-            self.i += 1
+        if self.head is None:
             self.head = self.tail = new_node
-            self.head.prev_node = None
-            self.tail.next_node = None
-            print('-----------------------------------')
-            list.menu()
+            self.head.next = self.head.prev = self.tail.next = self.tail.prev = None
         else:
-            self.i += 1
-            self.head.prev_node = new_node
-            new_node.next_node = self.head
-            new_node.prev_node = None
+            self.head.prev = new_node
+            new_node.next = self.head
             self.head = new_node
-            print('-----------------------------------')
-            list.menu()
+            self.tail.next = None
     
-    def del_f (self, data):
-        if self.head == None:
-            print('-----------------------------------')
-            print ('Нечего удалять, спсисок пуст!')
-            print('-----------------------------------')
-            list.menu()
+    def pop(self):
+        if self.head is None:
+            retun (f"Нечего удалять")
         else:
-            self.i -= 1
-            if(self.head != self.tail):
-                self.tail = self.tail.prev_node 
-                self.tail.next_node = None
-                print('-----------------------------------')
-                list.menu()
-            else:   
-                self.head = self.tail = None
-                print('-----------------------------------')
-                list.menu()
-            
-    def del_s (self, data):   
-        if(self.head == None):   
-            print('-----------------------------------')
-            print ('Нечего удалять, спсисок пуст!')
-            print('-----------------------------------')
-            list.menu()
+            if self.head is not self.tail:
+                self.data2 = self.tail.prev
+                del self.tail
+                self.tail = self.data2
+            else:
+                del self.tail
+                
+    def shift(self):
+        if self.head is None:
+            retun (f"Нечего удалять")
         else:
-            self.i -= 1
-            if(self.head != self.tail):   
-                self.head = self.head.next_node 
-                self.head.previous = None
-                print('-----------------------------------')
-                list.menu()
-            else:   
-                self.head = self.tail = None
-                print('-----------------------------------')
-                list.menu()
-    
-    def display (self):
-        current = self.head
-        if self.head == None:
-            print('-----------------------------------')
-            print('Двусвязный список пуст!')
-            print('-----------------------------------')
-            list.menu()
-        else:
-            print('-----------------------------------')
-            print('Наш двусвязный список:')
-            while current != None:
-                print(current.data),
-                current = current.next_node
-            print("Количество узлов - ", self.i)
-            print('-----------------------------------')
-            list.menu()
-            
-list = MyList()
-list.menu()
-
-
-
+            if self.head is not self.tail:
+                self.data2 = self.head.next
+                del self.head
+                self.head = self.data2
+            else:
+                del self.head
+        
+        
+nodes = NodeList()
+nodes.push(1)
+nodes.push(10)
+nodes.unshift(11)
+nodes.push(1000)
+nodes.push(200)
+nodes.pop()
+nodes.shift()
+print(nodes.head.data)
+print(nodes.tail.data)
+print(nodes.tail.prev.data)
+print(nodes.head.next.data)
 
 
